41af8dc6c39f7c4452377fc77e143ade40eeafb3
ntdkhang <ntdkhang143@gmail.com>
2023-12-04 18:18:04 -0600

    Initial Commit

---
+++ WhatsPoppin/WhatsPoppinApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsPoppinApp.swift
+//  WhatsPoppin
+//
+//  Created by Khang Nguyen on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsPoppinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
