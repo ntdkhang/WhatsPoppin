8c9379b9337a5865e5bc5a1f8c20fb9cf98a4d15
ntdkhang <ntdkhang143@gmail.com>
2023-12-04 18:51:12 -0600

    Gradient background

--- WhatsPoppin/BrowsePartyView.swift
+++ WhatsPoppin/BrowsePartyView.swift
@@ -10,52 +10,70 @@ import SwiftUI
 struct BrowsePartyView: View {
     @ObservedObject var partyVM: PartyViewModel = PartyViewModel()
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(partyVM.parties) { party in
-                    PartyCardView(party: party)
-                        .padding()
+        ZStack {
+            backgroundGradient
+                .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    ForEach(partyVM.parties) { party in
+                        PartyCardView(party: party)
+                            .padding()
+                    }
                 }
+                
             }
-            
         }
         .preferredColorScheme(.dark)
     }
     
+    var backgroundGradient: LinearGradient {
+            LinearGradient(colors: [.BGPink, .BGPurple, .BGPurple, .BGPink],
+                           startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
 }
 
 struct PartyCardView: View {
     var party: Party
     
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: party.imageURLString)) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(roundedRect)
-            } placeholder: {
-                ProgressView()
-            }
-            HStack {
-                Image(systemName: "calendar")
-                Group {
-                    Text(party.startDate, style: .date)
-                    Text(party.startDate, style: .time)
-                    if (party.endDate != nil) {
-                        Text("-")
-                        Text(party.endDate!, style: .time)
-                    }
+        ZStack {
+            VStack {
+                AsyncImage(url: URL(string: party.imageURLString)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(roundedRect)
+                } placeholder: {
+                    ProgressView()
                 }
-                .font(.subheadline)
+                HStack {
+                    Image(systemName: "calendar")
+                    Group {
+                        Text(party.startDate, style: .date)
+                        Text(party.startDate, style: .time)
+                        if (party.endDate != nil) {
+                            Text("-")
+                            Text(party.endDate!, style: .time)
+                        }
+                    }
+                    .font(.subheadline)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 8)
+                Text(party.name)
+                    .font(.title.bold())
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 8)
-            Text(party.name)
-                .font(.title.bold())
-        }
-        .padding(4)
+            .padding(4)
+            .background {
+                Color.black.opacity(0.3)
+                    .blur(radius: 8)
+                    .clipShape(roundedRect)
+            }
             
+            VStack(alignment: .leading) {
+                Text("Hello")
+            }
+        }
     }
     
     var roundedRect: RoundedRectangle {
---
+++ WhatsPoppin/Color+.swift
@@ -0,0 +1,19 @@
+//
+//  Color+.swift
+//  WhatsPoppin
+//
+//  Created by Khang Nguyen on 12/4/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var BGPink: Color {
+        Color("BGPink")
+    }
+    
+    static var BGPurple: Color {
+        Color("BGPurple")
+    }
+}
