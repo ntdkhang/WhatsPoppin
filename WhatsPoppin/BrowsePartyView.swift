433e589f059cf712cc702e1b8e19caaaf01ad579
ntdkhang <ntdkhang143@gmail.com>
2023-12-04 18:28:42 -0600

    basic model and view

---
+++ WhatsPoppin/BrowsePartyView.swift
@@ -0,0 +1,18 @@
+//
+//  BrowsePartyView.swift
+//  WhatsPoppin
+//
+//  Created by Khang Nguyen on 12/4/23.
+//
+
+import SwiftUI
+
+struct BrowsePartyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowsePartyView()
+}
---
+++ WhatsPoppin/Party.swift
@@ -0,0 +1,8 @@
+//
+//  Party.swift
+//  WhatsPoppin
+//
+//  Created by Khang Nguyen on 12/4/23.
+//
+
+import Foundation
